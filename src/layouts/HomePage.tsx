883b81290ea3f891b672f2c5cd59f29a094a84f5
John Rodrigues <john.rodrigues008@gmail.com>
2024-03-10 02:59:05 -0300

    Sending project to deploy

---
+++ src/layouts/HomePage.tsx
@@ -0,0 +1,11 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="text-2 w-full h-[100vh] flex justify-center items-center bg-black">
+      <h1 className="text-2xl text-cyan-300">Desafio Maeztra Dev</h1>
+    </div>
+  );
+};
+
+export default HomePage;
